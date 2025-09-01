1c72c38426548863f5c758ace93e3cc5fc2f964b
Amit Rawat <amit.rawat@blenheimchalcot.com>
2025-09-01 21:31:19 +0530

    Added some tests

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,6 +2,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
+  console.log("Test 01");
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
@@ -9,7 +10,51 @@ test('has title', async ({ page }) => {
 });
 
 test('get started link', async ({ page }) => {
+  console.log("Test 02");
   await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+test('Test 3', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  console.log("This test will fail");
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeDisabled();
+});
+
+
+test.describe("Describe grouping", () => {
+  test('Group Test 01', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+    console.log("Inside group, test 01");
+
+    await page.getByRole('link', { name: 'Get started' }).click();
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  });
+
+  test('Group Test 02', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+    console.log("Inside group, test 02");
+
+    await page.getByRole('link', { name: 'Get started' }).click();
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  });
+});
+
+
+test('Test 4', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  console.log("Outside the group,test 4, this should pass");
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
