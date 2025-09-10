59b93e710eeafebf309a188f2b11ba5410c80faa
Amit Rawat <amit.rawat@blenheimchalcot.com>
2025-09-10 21:05:05 +0530

    Github actions practice with PR

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,16 +1,16 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  console.log("Test 01");
+test('Independent Test 1', async ({ page }) => {
+  console.log("Independent Test 01");
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
-  console.log("Test 02");
+test('Independent Test 2', async ({ page }) => {
+  console.log("Independent Test 02");
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
@@ -20,7 +20,9 @@ test('get started link', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
 
-test('Test 3', async ({ page }) => {
+test('Independent Test 3', async ({ page }) => {
+
+  console.log("Independent Test 03");
   await page.goto('https://playwright.dev/');
 
   // console.log("This test will fail");
@@ -35,33 +37,35 @@ test('Test 3', async ({ page }) => {
 
 
 test.describe("Describe grouping", () => {
-  
+
   test('Group Test 01', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
+
     console.log("Inside group, test 01");
 
+    await page.goto('https://playwright.dev/');
     await page.getByRole('link', { name: 'Get started' }).click();
     await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
   });
 
   test('Group Test 02', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
     console.log("Inside group, test 02");
 
+    await page.goto('https://playwright.dev/');
     await page.getByRole('link', { name: 'Get started' }).click();
     await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
   });
 });
 
 
-test('Test 4', async ({ page }) => {
+test('Independent Test 4', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
-  console.log("Outside the group,test 4, this should pass");
+  console.log("Independent test 04");
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  console.log('$ git checkout -b "PYPD-YT"');
 });
